ac8c73447a935d98d73d2cbea32de54a0d378b61
Robert <fauverism@Roberts-MacBook-Pro.local>
2017-12-03 12:58:40 -0500

    :frog: resume work

--- resume.php
+++ resume.php
@@ -1,77 +1,155 @@
-
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package fauverism
- */
-
-get_header(); ?>
-
 	<div id="primary" class="content-area row">
 		<main id="main" class="site-main col-md-12" role="main">
-
       <div class="resume">
-        <h1>Resume of Robert E. Fauver</h1>
-        <small>Last updated June 30<sup>th</sup>, 2017</small>
+        <div class="row">
+          <div class="col-md-8">
+            <h1>Robert E. Fauver</h1>
+          </div>
+          <div class="col-md-4">
+            <small>Last updated December 4th, 2017</small>
+          </div> 
+        </div>             
+        <div class="row introduction">
+          <div class="col-md-12">
+            <p>Introduction, Lorem ipsum dolor sit amet consectetur adipisicing elit. A tempora animi autem. Dolore pariatur quasi veritatis, aut voluptate ea deserunt laboriosam voluptas esse quas. Dolores obcaecati debitis quaerat nostrum nam!</p>
+          </div>
+        </div>
+        
         <div class="row skills">
-          <div class="col-md-3"></div>  
+          <div class="col-md-12">
+            <h1>Skills</h1>
+          </div>
         </div>  
+
+        <div class="row technology">
+          <div class="col-md-12">
+            <h1>Technology</h1>
+          </div>
+        </div>
+        <div class="row tech">
+          <div class="col-md-3">
+            <ul>
+              <li>JavaScript</li>
+              <li>AngularJS</li>
+              <li>Node</li>
+              <li>Express</li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            <ul>
+              <li>CSS</li>
+              <li>CSS 3</li>
+              <li>SASS</li>
+              <li>LESS</li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            <ul>
+              <li>Design</li>
+              <li>Adobe Suite</li>
+              <li>Sketch</li>
+              <li>Omnigraffle</li>
+              <li>Fontographer</li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            <ul>
+              <li>Other</li>
+              <li>DB Visualizer</li>
+              <li>Vim (.vimrc)</li>
+              <li>WebStorm</li>
+              <li>Visual Studio Code</li>
+            </ul>
+          </div>
+        </div>
+
         <div class="row experience">
           <div class="col-md-12">
+            <h1>Experience</h1>
+          </div>
+        </div>
+        <div class="row experience comcast">
+          <div class="col-md-12">
             <div class="row role company">
-              <div class="col-md-9">
+              <div class="col-md-8">
                 <h3><em>Software Enigeer</em>, Comcast</h3>
               </div>
-              <div class="col-md-3">
-                <h5>Jan. 2015 &em; Present</h5>
+              <div class="col-md-4">
+                <h4>Jan. 2015 - Present</h4>
               </div>
             </div>
             <div class="row description">
               <div class="col-md-12">
                 <p>Work in site relability eng group, types of apps, what they do, </p>
-              </div>
-            </div>
-            <div class="row description">
-              <div class="col-md-6">
-                <ul>
-                  <li>Technology</li>
-                  <li>Lorem</li>
-                </ul>
-              </div>
-              <div class="col-md-6">
-                <ul>
-                  <li>Ipsum</li>
-                  <li>Dolor</li>
-                </ul>
+                <p>How technology is used</p>
               </div>
             </div>
           </div>
-          <!--Timeline?
-          Business, Location, timeframe, short description, duties
-         Data Visualizations -->
         </div>  
-        <div class="row education">
-         
-        </div>  
-        <div class="row social">
-         
-        </div>  
-        <div class="row contact">
-         
-        </div>  
-      </div>
+        <div class="row experience acurian">
 
+        </div>
+        <div class="row experience nbme">
+
+        </div>
+        <div class="row experience bms">
+
+        </div>
+        <div class="row experience ml">
+        
+        </div>  
+        <div class="row experience true-action">
+
+        </div>
+        <div class="row experience princeton-university">
+
+        </div>
+        <div class="row experience princeton-online">
+
+        </div>
+        <div class="row experience other">
+
+        </div>
+
+        <div class="row education">
+          <div class="col-md-12">
+            <h1>Education</h1>
+          </div>
+        </div>
+        <div class="row education aiph-moorestown">
+         
+        </div>   
+
+        <div class="row misc">
+          <div class="col-md-12">
+            <h1>Misc</h1>
+          </div>         
+        </div>  
+
+        <div class="row contact">
+          <div class="col-md-12">
+            <h1>Contact</h1>
+          </div>         
+        </div>  
+        
+        <div class="row social">
+          <div class="col-md-12">
+            <h1>Social</h1>
+          </div>
+        </div> 
+
+        <div class="row download">
+          <div class="col-md-12">
+            <h1>Download</h1>
+          </div>
+        </div>  
+
+    <!--
+        Timeline?
+        Business, Location, timeframe, short description, duties
+        Data Visualizations
+    -->
+
+      </div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
